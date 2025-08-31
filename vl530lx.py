99d3f1f20adb88b3aaf91bd453680e5601b721d6
Adideb Das <75713088+adideb-das@users.noreply.github.com>
2025-08-31 16:49:41 +0530

    Update vl530lx.py

--- vl530lx.py
+++ vl530lx.py
@@ -2,11 +2,10 @@ import time, board, busio
 import adafruit_vl53l0x
 
 i2c = busio.I2C(board.SCL, board.SDA)
-tof = adafruit_vl53l0x.VL53L0X(i2c)     # i2c address of lidar is  0x29
+tof = adafruit_vl53l0x.VL53L0X(i2c)     
 
 
 while True:
-    # Distance is in centimeters
     dist = tof.range
     print(f"{dist} mm")
     time.sleep(0.1)
